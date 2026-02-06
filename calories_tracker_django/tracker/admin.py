8925259a68b99b42414dc83160ef8f852bf01e2a
hanan-vm7 <mohammedhananvm7@gmail.com>
2026-02-06 20:10:48 +0530

    Delete calories_tracker_django directory

--- calories_tracker_django/tracker/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Food, MealLog
-
-admin.site.register(Food)
-admin.site.register(MealLog)
